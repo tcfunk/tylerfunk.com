e9b8fbfdd75b3bd7364c573110386d294870ce86
Tyler Funk <tcfunk25@gmail.com>
2021-02-08 08:34:49 -0600

    adding nav links

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,25 +15,63 @@ const SocialLinkTwitch = "hover:from-twitch hover:to-twitch hover:text-white"
 const SocialIcon = "h-6 w-6"
 
 const NavWrapper = styled.section.attrs({
-  className: "container mx-auto py-2 flex flex-row-reverse"
+  // className: "container mx-auto py-2 flex flex-row-reverse"
+  className: `
+    container
+    mx-auto
+  `
 })``
+
+const NavList = styled.div.attrs({
+  className: `
+    flex
+    justify-center
+  `
+})``
+
+const NavLink = `
+  text-gray-100
+  text-lg
+  px-4
+`
 const HeaderMenuIcon = "text-gray-200 h-8 w-8"
 
 const TitleWrapper = styled.section.attrs({
-  className: "w-full relative mb-16"
+  className: `
+    w-full
+    relative
+    mb-16
+  `
 })``
 const TitleContainer = styled.section.attrs({
-  className: "container mx-auto py-2 relative z-10"
+  className: `
+    container
+    mx-auto
+    text-center
+  `
+  // className: "container mx-auto py-2 relative z-10"
 })``
 const TitleBackdrop = styled.div.attrs({
-  className: "h-full w-2/3 bg-gray-700 absolute left-0 top-0 z-0"
+  // className: "h-full w-2/3 bg-gray-700 absolute left-0 top-0 z-0"
 })``
 
 const NameTitle = styled.h1.attrs({
-  className: "font-serif text-7xl text-yellow-300"
+  className: `
+    font-serif
+    text-4xl
+    md:text-6xl
+    xl:text-7xl
+    text-yellow-300
+  `
 })``
 const NameSubtitle = styled.h2.attrs({
-  className: "font-serif text-4xl text-yellow-300"
+  className: `
+    font-serif
+    text-xl
+    md:text-2xl
+    xl:text-4xl
+    text-yellow-300
+  `
 })``
 
 class Header extends React.Component {
@@ -41,8 +79,6 @@ class Header extends React.Component {
     return (
       <header>
         <div>
-
-          <NavWrapper><HiOutlineMenuAlt3 className={HeaderMenuIcon}/></NavWrapper>
     
           <TitleWrapper>
             <TitleContainer>
@@ -52,11 +88,16 @@ class Header extends React.Component {
             <TitleBackdrop />
           </TitleWrapper>
 
-          {/* <ul>
-            <li><Link to="/">Portfolio</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/make-contact">Contact</Link></li>
-          </ul> */}
+          <NavWrapper>
+            {/* <HiOutlineMenuAlt3 className={HeaderMenuIcon}/> */}
+
+            <NavList>
+              <Link className={NavLink} to="/">Home</Link>
+              <Link className={NavLink} to="/about">About</Link>
+              <Link className={NavLink} to="/make-contact">Contact</Link>
+            </NavList>
+          </NavWrapper>
+
 
           {/* <section className="flex justify-center">
             <SocialLink className={[SocialLink, SocialLinkInstagram].join(' ')} href="https://www.instagram.com/tcfunk25/" target="_blank">
